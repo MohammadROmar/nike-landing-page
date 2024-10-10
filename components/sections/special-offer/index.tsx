3821385c86bd2ac1218509d0a24bfb84f92466c3
MohammadROmar <149619529+MohammadROmar@users.noreply.github.com>
2024-10-10 16:47:30 +0300

    Finished special offer section

--- components/sections/special-offer/index.tsx
+++ components/sections/special-offer/index.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image';
+
+import Button from '@/components/ui/button';
+import { offer } from '@/assets/images';
+import { arrowRight } from '@/assets/icons';
+
 export default function SpecialOffer() {
-  return <section className="padding">SpecialOffer</section>;
+  return (
+    <section className="padding max-container flex justify-center items-center max-xl:flex-col-reverse gap-8">
+      <Image
+        src={offer}
+        alt="Special offer!"
+        className="object-contain max-xl:w-full"
+      />
+
+      <div className="flex-1">
+        <h2 className="mt-8 font-palanquin text-4xl font-bold">
+          <span className="text-coral-red">Special </span>
+          Offer
+        </h2>
+        <p className="mt-4 info-text">
+          Embark on a shipping journey that redefines your experience with
+          unbeatable deals. Form premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 mb-11 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of expectional.
+        </p>
+        <div className="flex items-center gap-4">
+          <Button label="Shop Now!" icon={arrowRight} />
+          <Button label="Learn more" secondary />
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,11 +4,18 @@ type ButtonProps = {
   label: string;
   icon?: string;
   alt?: string;
+  secondary?: boolean;
 };
 
-export default function Button({ label, icon, alt }: ButtonProps) {
+export default function Button({ label, icon, alt, secondary }: ButtonProps) {
+  const textColor = secondary ? 'border-slate-gray' : 'text-white';
+  const bgColor = secondary ? 'bg-transparent' : 'bg-coral-red';
+  const borderColor = secondary ? 'border-slate-gray' : 'border-coral-red';
+
   return (
-    <button className="w-fit flex justify-center items-center gap-2 px-8 py-4 font-montserrat border border-coral-red text-lg leading-none bg-coral-red rounded-full text-white">
+    <button
+      className={`w-fit flex justify-center items-center gap-2 px-8 py-4 font-montserrat border text-lg leading-5 rounded-full ${textColor} ${bgColor} ${borderColor}`}
+    >
       {label}
       {icon && (
         <Image
