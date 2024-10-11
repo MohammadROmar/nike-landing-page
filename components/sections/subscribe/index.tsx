5ddfa9d8d0622185f43bcc4a78a9e1cd517c7b6c
MohammadROmar <149619529+MohammadROmar@users.noreply.github.com>
2024-10-11 09:15:29 +0300

    Finished newsletter section

--- components/sections/subscribe/index.tsx
+++ components/sections/subscribe/index.tsx
@@ -1,5 +1,23 @@
+import Button from '@/components/ui/button';
+
 export default function Subscribe() {
   return (
-    <section className="padding-x sm:py-32 py-16 w-full">Subsscribe</section>
+    <section
+      id="contact-us"
+      className="padding-x sm:py-32 py-16 max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[4.25rem] font-palanquin font-bold">
+        Sign Up For <span className="text-coral-red">Updates </span> &
+        Newsletter
+      </h3>
+      <div className="lg:min-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input
+          type="email"
+          placeholder="subscribe@nike.com"
+          className="input"
+        />
+        <Button label="Sign Up" />
+      </div>
+    </section>
   );
 }
